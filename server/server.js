f90d3b2b77c8a2ed0a2a8fda2266c391627ee36e
Ayorinde <ajibaye@gmail.com>
2023-06-15 11:36:09 +0100

    #fix: deloyment issues using aws elastic beanstalk

--- server/server.js
+++ server/server.js
@@ -1,35 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const { NODE_ENV } = process.env;
-const fs = require("fs");
-// const path = require('path');
-
-const UPLOADS_DIR = "../uploads";
-
-// create the uploads directory if it doesn't exist
-if (!fs.existsSync(UPLOADS_DIR)) {
-  fs.mkdirSync(UPLOADS_DIR);
-}
+// const { NODE_ENV } = process.env;
 
 const app = require("./app");
 
 /* Loading the environment variables from the .env file. */
 require("dotenv").config();
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 4001;
 
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(
-    process.env.MONGO_URL,
-
-    // dev only
-    () => {
-      if (NODE_ENV === 'development') {
-        console.log("Database connection successful \n ");
-      }
-    }
-  )
+  .connect(process.env.MONGO_URL)
   .then(app.listen(port), console.log(`server running on port ${port}`))
   .catch((err) => console.log(err));
